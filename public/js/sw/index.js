935167c33c7a62c55da4ee764de3e1a570fb28cd
Elizabeth Lei <beth.a.lei@gmail.com>
2018-01-16 18:32:02 -0500

    respond to all requests with an html response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', function(event) {
-  console.log('hello', event.request);
+  // TODO: respond to all requests with an html response
+  // containing an element with class="a-winner-is-me".
+  // Ensure the Content-Type of the response is "text/html"
+  event.respondWith(
+    new Response('<strong class="a-winner-is-me">Hello World!</strong>', {
+      headers: {'Content-Type': 'text/html'}
+    })
+  )
 });
