b14ec56077627c82239919e1dad23edb7be35f21
Elizabeth Lei <beth.a.lei@gmail.com>
2018-01-22 20:00:16 -0500

    service worker update notification

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -12,13 +12,57 @@ export default function IndexController(container) {
 }
 
 IndexController.prototype._registerServiceWorker = function() {
-  // TODO: register service worker
   if (!navigator.serviceWorker) return;
 
-  navigator.serviceWorker.register('/sw.js').then(function() {
-    console.log('Registration worked!');
-  }).catch(function() {
-    console.log('Registration failed!');
+  var indexController = this;
+
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    // TODO: if there's no controller, this page wasn't loaded
+    // via a service worker, so they're looking at the latest version.
+    // In that case, exit early
+    if (!navigator.serviceWorker.controller) {
+      return;
+    }
+
+    // TODO: if there's an updated worker already waiting, call
+    // indexController._updateReady()
+    if (reg.waiting) {
+      indexController._updateReady();
+      return;
+    }
+
+    // TODO: if there's an updated worker installing, track its
+    // progress. If it becomes "installed", call
+    // indexController._updateReady()
+    if (reg.installing) {
+      indexController._trackInstalling(reg.installing);
+      return;
+    }
+
+    // TODO: otherwise, listen for new installing workers arriving.
+    // If one arrives, track its progress.
+    // If it becomes "installed", call
+    // indexController._updateReady()
+    reg.addEventListener('updatefound', function() {
+      indexController._trackInstalling(reg.installing);
+      return;
+    })
+  });
+};
+
+IndexController.prototype._trackInstalling = function(worker) {
+  var indexController = this;
+
+  worker.addEventListener('statechange', function() {
+    if (worker.state == 'installed') {
+      indexController._updateReady();
+    }
+  })
+}
+
+IndexController.prototype._updateReady = function() {
+  var toast = this._toastsView.show("New version available", {
+    buttons: ['whatever']
   });
 };
 
